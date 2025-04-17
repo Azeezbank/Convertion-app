9e650ba83063f5f4f354c7bd147d98f951dd59ca
Azeezbank <159575592+Azeezbank@users.noreply.github.com>
2025-04-17 07:53:27 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   const [conversionRate, setConversionRate] = useState("");
 
   //Fetch convertion rate
-  const handleRate = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleRate = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     try {
       const response = await fetch("/api/conversion_rate", {
